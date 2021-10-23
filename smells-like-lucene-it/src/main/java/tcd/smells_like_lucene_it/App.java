f735d63863ded1e41d73c0b611b337ddc23fbcd0
David Dunne <50323487+ddunne6@users.noreply.github.com>
2021-10-23 17:52:20 +0100

    access to every file for Financial Times

--- smells-like-lucene-it/src/main/java/tcd/smells_like_lucene_it/App.java
+++ smells-like-lucene-it/src/main/java/tcd/smells_like_lucene_it/App.java
@@ -1,13 +1,22 @@
 package tcd.smells_like_lucene_it;
+import static tcd.constants.FilePathPatterns.*;
 
-/**
- * Hello world!
- *
- */
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        // Parse Each Document for Financial Times
+        for (String pattern : FT_PATTERNS) {
+        	System.out.println("First file >>> " + getFinancialTimesFile(pattern, 1));
+        	int index = 1;
+        	while(Files.exists(Paths.get(getFinancialTimesFile(pattern, index)))) {
+        		System.out.println(getFinancialTimesFile(pattern, index));
+        		index++;
+        	}
+        }
     }
 }
