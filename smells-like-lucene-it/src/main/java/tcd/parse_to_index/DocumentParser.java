ef805eb61d6562a4c9e5cd54b2600707528b5aa6
David Dunne <50323487+ddunne6@users.noreply.github.com>
2021-10-23 16:41:16 +0100

    java project setup

---
+++ smells-like-lucene-it/src/main/java/tcd/parse_to_index/DocumentParser.java
@@ -0,0 +1,10 @@
+package tcd.parse_to_index;
+
+import java.util.List;
+
+public class DocumentParser {
+	// Parse file and return list of documents
+	public List <CustomDocument> parse(String filePath) {
+		return null;
+	}
+}
---
+++ smells-like-lucene-it/src/main/java/tcd/smells_like_lucene_it/App.java
@@ -0,0 +1,13 @@
+package tcd.smells_like_lucene_it;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
