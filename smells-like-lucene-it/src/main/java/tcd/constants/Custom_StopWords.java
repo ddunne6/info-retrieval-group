08049acd7f6653c128ff399387df46a3c94b1d14
saubhagyasharma99 <92671977+saubhagyasharma99@users.noreply.github.com>
2021-11-26 12:30:49 +0000

    Update Custom_StopWords.java

--- smells-like-lucene-it/src/main/java/tcd/constants/Custom_StopWords.java
+++ smells-like-lucene-it/src/main/java/tcd/constants/Custom_StopWords.java
@@ -18,38 +18,57 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;  
 public class Custom_StopWords {
 	private   final static Path currentRelativePath = Paths.get("").toAbsolutePath();
-	static List<String>  stopwords_list =new ArrayList<String>(); 
-public  static String[] getStopWords(float a) throws EncryptedDocumentException, IOException {
+	static List<String>  stopWords_list =new ArrayList<String>(); 
+	static String[] exception;
+	// throws EncryptedDocumentException, IOException
+public  static String[] getStopWords(float threshold) {
+	try {
 		
 		//Currently made of words that appear in more than 70% of the documents
 		
 		//Workbook wb = WorkbookFactory.create(new File("C:\\Users\\Saubhagya\\Desktop\\sample-code\\STOPWORD_LIST.xlsx"));
-		Workbook wb = WorkbookFactory.create(new File(currentRelativePath+"/STOPWORD_LIST.xlsx"));
+		 Workbook wb = WorkbookFactory.create(new File(currentRelativePath+"/STOPWORD_LIST.xlsx"));
 		 DataFormatter df = new DataFormatter();
-		 String[] list_stopwords = null;
-		 Sheet s = wb.getSheetAt(0);
-		 
-		 for (int i = 1; i < 500; i++) {
+		 String[] list_stopWords = null;
+	   	 Sheet s = wb.getSheetAt(0);
+	   	 int rowCount= s.getLastRowNum();
+	
+		 for (int i = 1; i < rowCount; i++) {
 			 Row r1 = s.getRow(i);
 			 Cell cA1 = r1.getCell(3);
+			 
 			
 			 String asItLooksInExcel = df.formatCellValue(cA1);
 			 float fre = Float.parseFloat(asItLooksInExcel);
 			
-			 if(fre>=a) {
+			 if(fre>=threshold) {
 			
 				 Cell ca2=r1.getCell(0);
 				 String asItLooksInExcel2 = df.formatCellValue(ca2);
-				 stopwords_list.add(asItLooksInExcel2);
+				 stopWords_list.add(asItLooksInExcel2);
 				 
 			 }
-			list_stopwords = stopwords_list.toArray(new String[0]);
+			list_stopWords = stopWords_list.toArray(new String[0]);
 			
 			}
 		
-		return list_stopwords;
+		return list_stopWords;
+	}catch (Exception e) {
+		return exception;
+	}
 		
 	}
+
+//public static void main(String []args) 
+
+//{
+//	try {
+//	System.out.println(Arrays.asList(getStopWords(90)));
+//	}catch (Exception e) {
+//		System.out.println(Arrays.asList(getStopWords(90)));
+//	}
+	
+//}
 
 
 
