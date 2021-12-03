39ffed35f4fb1846ab0fc9df7f9b00e335e00c4b
Ciara Gilsenan <41157101+CiaraG98@users.noreply.github.com>
2021-12-03 12:44:23 +0000

    Geo mapping ciara - New cities in Synonym Mapping & Countries geo boosting -- MAP -> 0.3334 (#40)
    * add new cities to synonym-mapping
    
    * city geo-boost does nothing to map
    
    * change to row[1]

--- smells-like-lucene-it/src/main/java/tcd/generate_queries/CreateQuery.java
+++ smells-like-lucene-it/src/main/java/tcd/generate_queries/CreateQuery.java
@@ -59,7 +59,7 @@ public class CreateQuery {
 	private Float topicTitleBoost = 4f;
 	private Float topicDescriptionBoost = 2.5f;
 	private Float topicNarrativeBoost = 1f;
-	private Float geoBoost = 1f;
+	private Float geoBoost = 2f;
 	
 	public CreateQuery(String runName, Similarity runSimilarity) {
 		this.runName=runName;
@@ -212,26 +212,26 @@ public class CreateQuery {
 					mustNotNarr += ".";
 			}
 			
-			String geoTitle = "";
-			String geoDescription = "";
-			String geoString = "";
+			String geoCountriesTitle = "";
+			String geoCountriesDescription = "";
+			String geoCountriesString = "";
 			
 			for (String name : geoNames) {
 				if(title.contains(name))
 				{
 					//geoTitle += name+" ";
-					geoString += name+" ";
-					if (!geoString.contains(name)) {
-						geoString += name+" ";
+					//geoCountriesString += name+" ";
+					if (!geoCountriesString.contains(name)) {
+						geoCountriesString += name+" ";
 					}
 					//System.out.println(geoTitle);
 				}
 				if(description.contains(name))
 				{
-					geoDescription += name+" ";
+					geoCountriesDescription += name+" ";
 					
-					if (!geoString.contains(name)) {
-						geoString += name+" ";
+					if (!geoCountriesString.contains(name)) {
+						geoCountriesString += name+" ";
 					}
 					//System.out.println(geoDescription);
 				}
@@ -258,11 +258,10 @@ public class CreateQuery {
 			newBooleanQuery.add(boostedTopicDescription, BooleanClause.Occur.SHOULD);
 			newBooleanQuery.add(boostedTopicNarrative, BooleanClause.Occur.SHOULD);
 			
-			if (! geoString.equals("")) {
-				Query geoQuery = multiqp.parse(MultiFieldQueryParser.escape(geoString));
+			if (!geoCountriesString.equals("")) {
+				Query geoQuery = multiqp.parse(MultiFieldQueryParser.escape(geoCountriesString));
 				Query boostedGeoQuery = new BoostQuery(geoQuery, geoBoost);
-				//newBooleanQuery.add(boostedGeoQuery, BooleanClause.Occur.SHOULD);
-				
+				newBooleanQuery.add(boostedGeoQuery, BooleanClause.Occur.SHOULD);
 			}
 			
 			Query newQuery = newBooleanQuery.build();
--- smells-like-lucene-it/src/main/java/tcd/mappings/MySynonymMap.java
+++ smells-like-lucene-it/src/main/java/tcd/mappings/MySynonymMap.java
@@ -7,6 +7,7 @@ import org.apache.lucene.util.CharsRef;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -15,32 +16,24 @@ import java.io.FileNotFoundException;
 
 public class MySynonymMap{
     // UK/US synonyms from http://www.tysto.com/uk-us-spelling-list.html
-    // Capitals: https://www.countries-ofthe-world.com/capitals-of-the-world.html
-    private static final String CITIES_FILE = "../cities.txt";
+    private static final String CITIES_FILE = "../worldcities.csv";
     private static final String UK_US_FILE = "../uk_us.txt";
-    public SynonymMap createSynonymMap() throws FileNotFoundException {
+    public SynonymMap createSynonymMap() throws IOException {
         SynonymMap synMap = new SynonymMap(null, null, 0);
-        Scanner readInput = new Scanner(new File(CITIES_FILE));
-        Scanner input2 = new Scanner(new File(UK_US_FILE));
+        Scanner ukUsInput = new Scanner(new File(UK_US_FILE));
+        BufferedReader citiesReader = new BufferedReader(new FileReader(CITIES_FILE));
 
         try {
         	final SynonymMap.Builder builder = new SynonymMap.Builder(true);
-        	
-        	while(readInput.hasNext()){
-                String[] capitalAndCountry = readInput.nextLine().split(" ");
-                //System.out.println(Arrays.toString(capitalAndCountry));
-                String output = "";
-                if(capitalAndCountry.length == 3) {
-                	output = capitalAndCountry[1] + " " + capitalAndCountry[2];
-                	//System.out.println(output);
-                }
-                else {
-                	output = capitalAndCountry[1];
-                }
-                builder.add(new CharsRef(capitalAndCountry[0]), new CharsRef(output), true);
+        	String row;
+        	while((row = citiesReader.readLine()) != null){
+                String[] rowData = row.split(",");
+                //System.out.println("City: " + rowData[0] + " Country: " + rowData[4]);
+                
+                builder.add(new CharsRef(rowData[1].toLowerCase()), new CharsRef(rowData[4].toLowerCase()), true);
             }
-            while(input2.hasNext()){
-                String[] ukAndUsMapping = input2.nextLine().split(" ");
+            while(ukUsInput.hasNext()){
+                String[] ukAndUsMapping = ukUsInput.nextLine().split(" ");
                 builder.add(new CharsRef(ukAndUsMapping[0]), new CharsRef(ukAndUsMapping[1]), true);
             }
         	synMap = builder.build();
@@ -48,6 +41,8 @@ public class MySynonymMap{
         } catch (Exception e) {
                 e.printStackTrace();
         }
+        citiesReader.close();
+        ukUsInput.close();
         return synMap;
     }
 }
